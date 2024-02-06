57a2070ef9c02337e2c67fe5008ab13cf3aac2a0
Pablo Dias <pabllosoarez@gmail.com>
2024-02-06 11:35:41 -0300

    Initial commit

---
+++ src/main/java/dev/pablodias/nlw_certification/NlwCertificationApplication.java
@@ -0,0 +1,13 @@
+package dev.pablodias.nlw_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwCertificationApplication.class, args);
+	}
+
+}
